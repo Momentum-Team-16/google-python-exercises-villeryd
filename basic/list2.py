01d9e227791ca0ff3c874eb16205e0daf43b88a6
Daniel <danielvillery@gmail.com>
2022-12-07 10:45:01 -0500

    kinda geniuses, brian is pumping

--- basic/list2.py
+++ basic/list2.py
@@ -12,9 +12,22 @@
 # all adjacent == elements have been reduced to a single element,
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
+import time
+
+
 def remove_adjacent(nums):
-  # +++your code here+++
-  return
+    # +++your code here+++
+    if nums == []:
+        return nums
+    nums_copy = [nums[0]]
+
+    i = 1
+    while i < len(nums):
+        if nums[i] != nums[i-1]:
+            nums_copy.append(nums[i])
+        i += 1
+
+    return nums_copy
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -22,8 +35,38 @@ def remove_adjacent(nums):
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
 def linear_merge(list1, list2):
-  # +++your code here+++
-  return
+    # +++your code here+++
+    # look at the two sorted piles
+    # compare each value
+    # add the lower value to the third pile and increment the pile that we took from until both piles are empty
+    start_time = time.time()
+    list1_count = 0
+    list2_count = 0
+    sorted_List = []
+
+    while list1_count < len(list1) and list2_count < len(list2):
+        if list1[list1_count] < list2[list2_count]:
+            sorted_List.append(list1[list1_count])
+            list1_count += 1
+
+        elif list1[list1_count] == list2[list2_count]:
+            sorted_List.append(list1[list1_count])
+            sorted_List.append(list2[list2_count])
+            list2_count += 1
+            list1_count += 1
+
+        else:
+            sorted_List.append(list2[list2_count])
+            list2_count += 1
+
+    if list1_count == len(list1):
+        endtime = time.time()
+        print(endtime - start_time)
+        return sorted_List + list2[list2_count:]
+    else:
+        endtime = time.time()
+        print(endtime - start_time)
+        return sorted_List + list1[list1_count:]
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
@@ -37,29 +80,31 @@ def linear_merge(list1, list2):
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
-  if got == expected:
-    prefix = ' OK '
-  else:
-    prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
 # Calls the above functions with interesting inputs.
 def main():
-  print 'remove_adjacent'
-  test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
-  test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
-  test(remove_adjacent([]), [])
+    print('remove_adjacent')
+    test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
+    test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
+    test(remove_adjacent([]), [])
 
-  print
-  print 'linear_merge'
-  test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
-       ['aa', 'bb', 'cc', 'xx', 'zz'])
-  test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
-       ['aa', 'bb', 'cc', 'xx', 'zz'])
-  test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
-       ['aa', 'aa', 'aa', 'bb', 'bb'])
+    print
+    print('linear_merge')
+    test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+         ['aa', 'bb', 'cc', 'xx', 'zz'])
+    test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+         ['aa', 'bb', 'cc', 'xx', 'zz'])
+    test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+         ['aa', 'aa', 'aa', 'bb', 'bb'])
+    test(linear_merge(['aa', 'bb', 'cc'], ['aa', 'bb', 'cc']),
+         ['aa', 'aa', 'bb', 'bb', 'cc', 'cc'])
 
 
 if __name__ == '__main__':
-  main()
+    main()
