3444583221761d21ae2492858faa7c6eae1ba88b
Daniel <danielvillery@gmail.com>
2022-12-08 09:43:29 -0500

    need to remove punctuation

--- basic/wordcount.py
+++ basic/wordcount.py
@@ -38,31 +38,56 @@ print_words() and print_top().
 """
 
 import sys
-
+import string
 # +++your code here+++
 # Define print_words(filename) and print_top(filename) functions.
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
 
+
+def print_words(filename): 0
+   with open(filename) as my_file:
+        big_list = my_file.read()
+    lowered_list = big_list.lower()
+    split_list = lowered_list.split()
+    word_count = {}
+
+    for word in split_list:
+        if word not in word_count.keys():
+            word_count[word] = split_list.count(word)
+
+    sorted_counts = dict(sorted(word_count.items()))
+
+    for word, count in sorted_counts.items():
+        print(f'{word} {count}')
+    return
+
+
+def print_top(filename):
+    return
+
+
 ###
+
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 def main():
-  if len(sys.argv) != 3:
-    print 'usage: ./wordcount.py {--count | --topcount} file'
-    sys.exit(1)
+    if len(sys.argv) != 3:
+        print('usage: ./wordcount.py {--count | --topcount} file')
+        sys.exit(1)
 
-  option = sys.argv[1]
-  filename = sys.argv[2]
-  if option == '--count':
-    print_words(filename)
-  elif option == '--topcount':
-    print_top(filename)
-  else:
-    print 'unknown option: ' + option
-    sys.exit(1)
+    option = sys.argv[1]
+    filename = sys.argv[2]
+    if option == '--count':
+        print_words(filename)
+    elif option == '--topcount':
+        print_top(filename)
+    else:
+        print('unknown option: ' + option)
+        sys.exit(1)
+
 
 if __name__ == '__main__':
-  main()
+    main()
